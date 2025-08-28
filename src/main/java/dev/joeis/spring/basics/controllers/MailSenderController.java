f3e8cf044c72bf118eec14279ad6bd008cb571d6
joe <josegarcia.dev@gmail.com>
2025-08-28 11:50:44 -0600

    Basics of dependecy injection

--- src/main/java/dev/joeis/spring/basics/controllers/MailSenderController.java
+++ src/main/java/dev/joeis/spring/basics/controllers/MailSenderController.java
@@ -2,12 +2,15 @@ package dev.joeis.spring.basics.controllers;
 
 import dev.joeis.spring.basics.mail.MailSender;
 import dev.joeis.spring.basics.mail.MailSenderImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MailSenderController {
-    MailSender mailSender = new MailSenderImpl();
+
+    @Autowired
+    MailSender mailSender;
 
     @RequestMapping("/send")
     public void sendMail() {
