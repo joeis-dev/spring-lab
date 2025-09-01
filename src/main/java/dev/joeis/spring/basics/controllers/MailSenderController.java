aace78fedb53d3679190db45a98cd45c2c57054e
joe <josegarcia.dev@gmail.com>
2025-09-01 11:55:36 -0600

    Injecting beans

--- src/main/java/dev/joeis/spring/basics/controllers/MailSenderController.java
+++ src/main/java/dev/joeis/spring/basics/controllers/MailSenderController.java
@@ -9,11 +9,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MailSenderController {
 
-    @Autowired
+    // method 1: injects the whole bean directly into this reference
+    // @Autowired
     MailSender mailSender;
+
+    // method 2: Injects only the required method from the bean referenced
+//    @Autowired
+//    public void setMailSender(MailSender mailSender) {
+//        this.mailSender = mailSender;
+//    }
+
+    // method 3: injects the bean in the constructor
+//    @Autowired
+//    public MailSenderController(MailSender mailSender) {
+//       this.mailSender = mailSender;
+//    }
+
+    // study case 1: N beans from the same interface can collide if we don't specify which one is the required.
+    // solution recommended: set bean's id when defined them as components -> @Component(name="myBeanId")
+    @Autowired
+    public MailSenderController(MailSender smtp) {
+        this.mailSender = smtp;
+    }
+
+    // alternative: use its bean's default ids (MailSMTP, MailPOP3 -> mailPOP3)
+//    @Autowired
+//    public MailSenderController(MailSender mailSMTP) {
+//        this.mailSender = mailSMTP;
+//    }
 
     @RequestMapping("/send")
     public void sendMail() {
-        mailSender.send("joe@joeis.dev", "Spring framework basics", "Lorem ipsum...");
+        mailSender.send("test@joeis.dev", "Spring framework basics", "Lorem ipsum...");
     }
 }
--- src/main/java/dev/joeis/spring/basics/mail/MailSMTP.java
+++ src/main/java/dev/joeis/spring/basics/mail/MailSMTP.java
@@ -2,14 +2,18 @@ package dev.joeis.spring.basics.mail;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
-public class SMTPImpl implements MailSender {
+// @Component: creates a default bean identified by mailSMTP
+// Recommended: define a custom id
+@Component("smtp")
+public class MailSMTP implements MailSender {
 
-    private static final Logger log = LoggerFactory.getLogger(SMTPImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(MailSMTP.class);
 
     @Override
     public void send(String to, String subject, String body) {
-        log.info("📤 Sending email... ");
+        log.info("📤 Sending email (SMTP protocol)... ");
         log.info("\uD83E\uDDD1\uD83C\uDFFD To: {}", to);
         log.info("\uD83D\uDCD5 Subject: {}", subject);
         log.info("\uD83D\uDCDC Body message: {}", body);
