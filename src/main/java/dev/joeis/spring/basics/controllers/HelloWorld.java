6138f858eca607c3c082ef47c3a082f57621378a
joe <josegarcia.dev@gmail.com>
2025-09-08 11:58:31 -0600

    Reading values from external properties

--- src/main/java/dev/joeis/spring/basics/controllers/HelloWorld.java
+++ src/main/java/dev/joeis/spring/basics/controllers/HelloWorld.java
@@ -1,13 +1,23 @@
 package dev.joeis.spring.basics.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
+    /**
+     * A better and scalable aproach is to create a centralized class with
+     * @ConfigurationPrefix(prefix="app.package.value") with their setters and getters so it will handle
+     * all values in app.package.*
+     * https://docs.spring.io/spring-boot/reference/features/external-config.html
+     * */
+    @Value("${app.env}")
+    String environment;
+
     @RequestMapping("/hello")
     public String helloWorld() {
-        return "Hello World from spring basics!";
+        return "Hello World from "+ environment + "!";
     }
 }
--- src/main/java/dev/joeis/spring/basics/controllers/MailSenderController.java
+++ src/main/java/dev/joeis/spring/basics/controllers/MailSenderController.java
@@ -4,6 +4,7 @@ import dev.joeis.spring.basics.mail.MailSender;
 import dev.joeis.spring.basics.mail.MailSenderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,62 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailSenderController {
     MailSender mailSender;
 
-    // method 1: injects the whole bean directly into this reference (there's only 1 bean defined
-    // for MailSender interface in the context app)
-    // @Autowired
-//    MailSender mailSender;
-
-    // method 2: Injects only the required method from the bean referenced
-//    @Autowired
-//    public void setMailSender(MailSender mailSender) {
-//        this.mailSender = mailSender;
-//    }
-
-    // method 3: injects the bean in the constructor
-//    @Autowired
-//    public MailSenderController(MailSender mailSender) {
-//       this.mailSender = mailSender;
-//    }
-
-    // study case 1: N beans from the same interface can collide if we don't specify which one is the required.
-    // solution recommended: set bean's id when defined them as components -> @Component(name="myBeanId")
-//    @Autowired
-//    public MailSenderController(MailSender smtp) {
-//        this.mailSender = smtp;
-//    }
-//
-    // alternative: use its bean's default ids (MailSMTP, MailPOP3 -> mailPOP3)
-//    @Autowired
-//    public MailSenderController(MailSender mailSMTP) {
-//        this.mailSender = mailSMTP;
-//    }
-
-    // Injection via @Primary: even if a custom bean id is set on smtp the priority to choose the right one
-    // suggest that MailSenderImpl will be picked as it has @Primary on its definition.
-//    @Autowired
-//    public MailSenderController(MailSender smtp) {
-//        this.mailSender = smtp;
-//    }
-
-    // Injection via @Qualifier: this ambiguity resolver overrides the default preference from @Primary
-    // and select the specific bean provided.
-//    @Autowired
-//    public MailSenderController(@Qualifier("myQualifierBean") MailSender mail) {
-//        this.mailSender = mail;
-//    }
-
     @Autowired
     public MailSenderController(MailSender mockMailFromConfig) {
         this.mailSender = mockMailFromConfig;
     }
 
-    /***
-     * There are others ways to inject dependencies like @Injection or @Resource but @Autowired becomes
-     * the facto and the recommended way to go from Spring Framework team. */
-
     @RequestMapping("/send")
-    public String sendMail() {
+
+    public String sendMail(@Value("${app.name}") String appName) {
         mailSender.send("test@joeis.dev", "Spring framework basics", "Lorem ipsum...");
-        return "Mail Sent";
+        return "Mail Sent from: " + appName;
     }
 }
