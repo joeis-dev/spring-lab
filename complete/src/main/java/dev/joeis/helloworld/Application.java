da7cb3693addc35ac1cd35dcbb8500001e13889a
Jose Garcia <josegarcia.dev@gmail.com>
2026-06-23 12:02:26 -0600

    New folder structure implemented for better review in the future

---
+++ complete/src/main/java/dev/joeis/helloworld/Application.java
@@ -0,0 +1,21 @@
+package dev.joeis.spring.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.GetMapping; 
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+    @GetMapping("hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+}
